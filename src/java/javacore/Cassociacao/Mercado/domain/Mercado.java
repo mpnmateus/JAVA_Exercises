61beab0d28b958875b9095c266e73e04a23fee70
Mateus Novo <mpnmateus@gmail.com>
2024-05-21 20:06:35 -0300

    Add método média valor Produtos. Uma prateleira pode ter, ou não, produtos, que por sua vez tem preços

--- src/java/javacore/Cassociacao/Mercado/domain/Mercado.java
+++ src/java/javacore/Cassociacao/Mercado/domain/Mercado.java
@@ -8,6 +8,49 @@ public class Mercado {
         this.prat2 = p2;
     }
 
+    //Método média valor dos produtos
+    public double mediaValorProdutos(){
+        double soma = 0;
+        int contador = 0;
+        //Prateleira 1
+        if (prat1 != null){
+            //Prateleira 1 -- Produto 1
+            if(prat1.getProd1() != null){
+                soma += prat1.getProd1().getPreco();
+                contador++;
+            }
+            //Prateleira 1 -- Produto 2
+            if (prat1.getProd2() != null){
+                soma += prat1.getProd2().getPreco();
+                contador++;
+            }
+            //Prateleira 1 -- Produto 3
+            if (prat1.getProd3() != null){
+                soma += prat1.getProd3().getPreco();
+                contador++;
+            }
+        }
+        //Prateleira 2
+        if (prat2 != null){
+            //Prateleira 2 -- Produto 1
+            if(prat2.getProd1() != null){
+                soma += prat2.getProd1().getPreco();
+                contador++;
+            }
+            //Prateleira 2 -- Produto 2
+            if (prat2.getProd2() != null){
+                soma += prat2.getProd2().getPreco();
+                contador++;
+            }
+            //Prateleira 2 -- Produto 3
+            if (prat2.getProd3() != null){
+                soma += prat2.getProd3().getPreco();
+                contador++;
+            }
+        }
+        return contador > 0 ? soma / contador : 0;
+
+    }
 
     //Getters
     public Prateleira getPrat1() {
