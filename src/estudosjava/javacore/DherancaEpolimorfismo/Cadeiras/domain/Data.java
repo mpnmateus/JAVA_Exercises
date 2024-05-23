ef836050f71b06c22fb74dade0bc67ed70bae462
Mateus Novo <mpnmateus@gmail.com>
2024-05-23 13:28:08 -0300

    Criando classe data

---
+++ src/estudosjava/javacore/DherancaEpolimorfismo/Cadeiras/domain/Data.java
@@ -0,0 +1,42 @@
+package estudosjava.javacore.DherancaEpolimorfismo.Cadeiras.domain;
+
+public class Data {
+    private int dia, mes, ano;
+
+    public Data (int dia, int mes, int ano){
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+    }
+
+    public String toString(){
+        return String.format(
+                "%d/%d/%d",
+                this.dia, this.mes, this.ano
+        );
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
