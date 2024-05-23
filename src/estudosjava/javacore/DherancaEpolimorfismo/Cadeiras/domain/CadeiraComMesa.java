1d69cfea7541d3c110786c3a60fedc1be7ebed78
Mateus Novo <mpnmateus@gmail.com>
2024-05-23 14:15:56 -0300

    Implementando toString() que herda toString de sua superclasse

--- src/estudosjava/javacore/DherancaEpolimorfismo/Cadeiras/domain/CadeiraComMesa.java
+++ src/estudosjava/javacore/DherancaEpolimorfismo/Cadeiras/domain/CadeiraComMesa.java
@@ -1,11 +1,35 @@
 package estudosjava.javacore.DherancaEpolimorfismo.Cadeiras.domain;
 
 public class CadeiraComMesa extends Cadeira {
-    private double largura, profundidade;
+    private double larguraMesa, profundidadeMesa; //Largura e Profundidade da mesa acoplada
 
     public CadeiraComMesa(Cor cor, double largura, double profundidade){
         super(cor);
-        this.largura = largura;
-        this.profundidade = profundidade;
+        this.larguraMesa = largura;
+        this.profundidadeMesa = profundidade;
+    }
+
+    @Override
+    public String toString(){
+        return String.format(
+                "Cadeira com Mesa: [Cor: %s, Largura: %f, Profundidade: %f]",
+                super.toString(), this.larguraMesa, this.profundidadeMesa
+        );
+    }
+
+    public double getLarguraMesa() {
+        return larguraMesa;
+    }
+
+    public void setLarguraMesa(double larguraMesa) {
+        this.larguraMesa = larguraMesa;
+    }
+
+    public double getProfundidadeMesa() {
+        return profundidadeMesa;
+    }
+
+    public void setProfundidadeMesa(double profundidadeMesa) {
+        this.profundidadeMesa = profundidadeMesa;
     }
 }
