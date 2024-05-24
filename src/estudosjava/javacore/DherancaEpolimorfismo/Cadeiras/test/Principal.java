1250cbe5c495ceaeecad63abe8ec135c5c8ab5c8
Mateus Novo <mpnmateus@gmail.com>
2024-05-23 21:28:54 -0300

    A partir do método main, seis alunos tentam entrar na sala, que so tem quatro cadeiras

--- src/estudosjava/javacore/DherancaEpolimorfismo/Cadeiras/test/Principal.java
+++ src/estudosjava/javacore/DherancaEpolimorfismo/Cadeiras/test/Principal.java
@@ -1,5 +1,9 @@
 package estudosjava.javacore.DherancaEpolimorfismo.Cadeiras.test;
 
+import javax.swing.*;
+import java.awt.Color;
+
+import estudosjava.javacore.DherancaEpolimorfismo.Cadeiras.domain.Aluno;
 import estudosjava.javacore.DherancaEpolimorfismo.Cadeiras.domain.Cadeira;
 import estudosjava.javacore.DherancaEpolimorfismo.Cadeiras.domain.Cor;
 import estudosjava.javacore.DherancaEpolimorfismo.Cadeiras.domain.Sala;
@@ -27,8 +31,31 @@ public class Principal {
                 Teclado.leInt("Digite o parâmetro G da 4ª cadeira: "),
                 Teclado.leInt("Digite o parâmetro B da 4ª cadeira: ")));
 
+        //Cria uma sala com todos parâmetros necessários
         Sala sala = new Sala(cad1, cad2, cad3, cad4);
 
+        //Imprime informações da sala
         System.out.println(sala);
+
+        for (int i = 1; i <= 6; i++){
+            Aluno aluno;
+            if (cad1.getAluno() == null || cad2.getAluno() == null || cad3.getAluno() == null ||
+                    cad4.getAluno() == null) {
+                aluno = new Aluno(Teclado.leString("\nDigite o nome do " + i + "º aluno: "),
+                        Teclado.leInt("Digite a idade do " + i + "º aluno: "));
+                if(i ==1)
+                    cad1.setAluno(aluno);
+                if(i ==2)
+                    cad2.setAluno(aluno);
+                if(i ==3)
+                    cad3.setAluno(aluno);
+                if(i ==4)
+                    cad4.setAluno(aluno);
+
+                System.out.println(aluno);
+            } else {
+                System.out.println("Não há mais lugar na sala para o " + i + "º aluno.");
+            }
+        }
     }
 }
